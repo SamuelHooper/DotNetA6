00d3e8e721d6a0239800667afda930b575d44e98
Samuel <grimr6666@gmail.com>
2023-03-03 16:39:04 -0600

    Add project files.

---
+++ Services/MainService.cs
@@ -0,0 +1,44 @@
+﻿using DotNetA6.Models;
+
+namespace ApplicationTemplate.Services
+{
+    public class MainService : IMainService
+    {
+
+        // Main Program code goes here
+        public void Invoke()
+        {
+            MediaContext mediaContext = new();
+            List<Media> mediaList = new();
+            string menuOption;
+
+            do
+            {
+                Console.WriteLine("\nSelect a media type to display:");
+                Console.WriteLine("1. Movies");
+                Console.WriteLine("2. Shows");
+                Console.WriteLine("3. Videos");
+                Console.WriteLine("Enter any other key to exit.\n");
+                menuOption = Console.ReadLine();
+
+                if (menuOption == "1")
+                {
+                    mediaList = mediaContext.Movies;
+                } else if (menuOption == "2")
+                {
+                    mediaList = mediaContext.Shows;
+                } else if (menuOption == "3")
+                {
+                    mediaList = mediaContext.Videos;
+                }
+
+                Console.WriteLine(); // Spacer line
+
+                foreach (Media media in mediaList)
+                {
+                    Console.WriteLine(media.Display());
+                }
+            } while (menuOption == "1" || menuOption == "2" || menuOption == "3");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationTemplate.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ApplicationTemplate
+{
+    internal class Startup
+    {
+        public IServiceProvider ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddLogging(builder =>
+            {
+                builder.AddConsole();
+                builder.AddFile("app.log");
+            });
+
+            // Add code here to register any interface and concrete service groups
+            services.AddSingleton<IMainService, MainService>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
